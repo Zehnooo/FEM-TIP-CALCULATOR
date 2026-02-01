ecf7ad4b6717dc775213aa67ce12a7f5b934d0d6
Zehno @ Home <126801501+Zehnooo@users.noreply.github.com>
2026-01-31 18:56:21 -0600

    Added js to hide the custom tip name and reveal the input

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("Hello World");
 requestAnimationFrame(() => document.body.classList.add('ready'));
+const custInputLabel = document.querySelector('#custom-tip-label');
+const elements = custInputLabel.children;
+const [custInput, custSpan] = elements;
+custInput.addEventListener('click', () => {
+    custSpan.classList.add('hide');
+    custInput.classList.add('reveal');
+    custInput.classList.remove('hide');
+});
+
